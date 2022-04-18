7adbcb3bdbcd40c79b6831b0836ad58bb083c7a4
asia_demartino@libero.it <asia_demartino@libero.it>
2022-04-18 23:28:45 +0200

    aggiunte funzioni per aggiungere e rimuovere carta

--- classi/utenti.php
+++ classi/utenti.php
@@ -11,6 +11,7 @@ class Utente {
     protected $indirizzo;
     protected $telefono;
     protected $sconto = 0;
+    protected $metodiPagamento = [];
 
     function __construct(string $nome, string $cognome, string $email, string $indirizzo, int $telefono){
         $this->nome = $nome;
@@ -18,6 +19,42 @@ class Utente {
         $this->email = $email;
         $this->indirizzo = $indirizzo;
         $this->telefono = $telefono;
+    }
+
+    public function addMetodoPagamento($numeroCarta,$dataScadenza,$cvv,$nomeIntestatario,$cognomeIntestatario,$circuito){
+        $date = explode("/",date("m/y"));
+        $scadenza = explode("/",$dataScadenza);
+        $mese = intval($date[0]);
+        $anno = intval($date[1]);
+        // var_dump($mese,$anno);
+        
+        if (intval($scadenza[1]) > $anno )  {
+            array_push($this->metodiPagamento,[
+                "numeroCarta"=>$numeroCarta,
+                "dataScadenza"=>$dataScadenza,
+                "cvv"=>$cvv,
+                "nomeIntestatario"=>$nomeIntestatario,
+                "cognomeIntestatario"=>$cognomeIntestatario,
+                "circuito"=>$circuito
+            ]);
+        } 
+        elseif (intval($scadenza[0]) >= $mese and intval($scadenza[1]) == $anno){
+            array_push($this->metodiPagamento,[
+                "numeroCarta"=>$numeroCarta,
+                "dataScadenza"=>$dataScadenza,
+                "cvv"=>$cvv,
+                "nomeIntestatario"=>$nomeIntestatario,
+                "cognomeIntestatario"=>$cognomeIntestatario,
+                "circuito"=>$circuito
+            ]);
+    }else {
+            echo "La tua carta è scaduta, inserisci un metodo di pagamento valido";
+        }
+        
+    }
+
+    public function removeMetodoPagamento(){
+        array_pop($this->metodiPagamento);
     }
 
 }
@@ -42,9 +79,9 @@ class UtenteRegistrato extends Utente {
 }
 
 $Mario = new Utente("Mario","Rossi","mariorossi@emai.it","Via di qui, 11",1234567890);
+$Mario->addMetodoPagamento("5488 4566 6128 7320","04/22","885","Mario","Rossi","Mastercard");
 var_dump($Mario);
-
-$Mario = new UtenteRegistrato("Mario","Rossi","mariorossi@emai.it","Via di qui, 11",1234567890,"1234");
+$Mario->removeMetodoPagamento();
 var_dump($Mario);
 
 ?>
