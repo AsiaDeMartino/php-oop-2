2ef905e510772cb28eae95aad5e3c57d1ba14052
asia_demartino@libero.it <asia_demartino@libero.it>
2022-04-18 20:25:28 +0200

    struttura iniziale

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- L’e-commerce vende prodotti per gli animali. I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+L’utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+Il pagamento avviene con la carta di credito, che non deve essere scaduta. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
